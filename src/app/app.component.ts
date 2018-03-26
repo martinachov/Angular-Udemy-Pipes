ea73bba395cffbf0bdf9c68feca1da71ba059ade
Martin Vacas <martinvacas@MacBook-Pro-de-infra-2.local>
2018-03-26 17:18:38 -0300

    Curso Angular 2 Udemy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,39 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+
+  nombre = "Martin";
+
+  nombre2 = "mARtin igNAcio vACAS";
+
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    edad: 500,
+    clave: "Wolverine",
+    direccion: {
+      calle:"Primera",
+      numero:1234
+    }
+  };
+
+  valorDePromesa = new Promise( (resolve, reject) => {
+    setTimeout( () => resolve('Llego la data'), 3500);
+  });
+
+  fecha = new Date();
+
+  video = 'lrRUF_uVMXM';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { CapitalizadoPipe} from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe
   ],
   imports: [
     BrowserModule
